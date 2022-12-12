e7d595848beae5c1bbedf5a172cf6314f47bd2a8
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-12-12 21:10:08 +0100

    Implemented ComplexExtensions.

--- 4-ExtensionMethods/Complex.cs
+++ 4-ExtensionMethods/Complex.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace ExtensionMethods
 {
     using System;
@@ -5,9 +7,6 @@ namespace ExtensionMethods
     /// <inheritdoc cref="IComplex"/>
     public class Complex : IComplex
     {
-        private readonly double re;
-        private readonly double im;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="Complex"/> class.
         /// </summary>
@@ -15,71 +14,73 @@ namespace ExtensionMethods
         /// <param name="im">the imaginary part.</param>
         public Complex(double re, double im)
         {
-            this.re = re;
-            this.im = im;
+            Real = re;
+            Imaginary = im;
         }
 
         /// <inheritdoc cref="IComplex.Real"/>
-        public double Real
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Real { get; }
 
         /// <inheritdoc cref="IComplex.Imaginary"/>
-        public double Imaginary
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Imaginary { get; }
 
         /// <inheritdoc cref="IComplex.Modulus"/>
-        public double Modulus
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Modulus => Math.Sqrt(Real * Real + Imaginary * Imaginary);
 
         /// <inheritdoc cref="IComplex.Phase"/>
-        public double Phase
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Phase => Math.Atan2(Imaginary, Real);
 
         /// <inheritdoc cref="IComplex.ToString"/>
         public override string ToString()
         {
-            // TODO improve
-            return base.ToString();
+            var stringBuilder = new StringBuilder();
+            double absImaginary = Math.Abs(Imaginary);
+
+            if (Equals(new Complex(0, 0)))
+            {
+                return "0";
+            }
+
+            if (!Real.Equals(0))
+            {
+                stringBuilder.Append($"{Real}");
+            }
+
+            if (!Imaginary.Equals(0))
+            {
+                stringBuilder.Append($" {GetComplexOperationSign} {absImaginary}");
+
+                // Removing excessive white-spaces or redundant operators.
+                if (Real.Equals(0))
+                {
+                    stringBuilder.Replace(" - ", "-");
+                    stringBuilder.Replace(" + ", "");
+                }
+            }
+
+            return stringBuilder.ToString();
         }
 
         /// <inheritdoc cref="IEquatable{T}.Equals(T)"/>
         public bool Equals(IComplex other)
         {
-            throw new System.NotImplementedException();
+            if (other is null) return false;
+
+            return Real.CompareTo(Math.Round(other.Real, 1)) == 0 &&
+                   Imaginary.CompareTo(Math.Round(other.Imaginary, 1)) == 0;
         }
 
         /// <inheritdoc cref="object.Equals(object?)"/>
         public override bool Equals(object obj)
         {
-            // TODO improve
-            return base.Equals(obj);
+            if (obj is IComplex complex) return Equals(complex);
+
+            return false;
         }
 
         /// <inheritdoc cref="object.GetHashCode"/>
-        public override int GetHashCode()
-        {
-            // TODO improve
-            return base.GetHashCode();
-        }
+        public override int GetHashCode() => HashCode.Combine(Real, Imaginary);
+
+        private string GetComplexOperationSign => Imaginary > 0 ? "+" : "-";
     }
 }
--- 4-ExtensionMethods/ComplexExtensions.cs
+++ 4-ExtensionMethods/ComplexExtensions.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace ExtensionMethods
 {
     /// <summary>
@@ -15,18 +13,18 @@ namespace ExtensionMethods
         /// <returns>the sum.</returns>
         public static IComplex Add(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            return new Complex(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
         }
 
         /// <summary>
-        /// Substract <paramref name="c2"/> from <paramref name="c1"/>.
+        /// Subtract <paramref name="c2"/> from <paramref name="c1"/>.
         /// </summary>
         /// <param name="c1">the first operand.</param>
         /// <param name="c2">the second operand.</param>
         /// <returns>the difference.</returns>
         public static IComplex Subtract(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            return new Complex(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
         }
 
         /// <summary>
@@ -37,7 +35,17 @@ namespace ExtensionMethods
         /// <returns>the product.</returns>
         public static IComplex Multiply(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            var first = c1.Real * c2.Real;
+            var outer = c1.Real * c2.Imaginary;
+            var inner = c1.Imaginary * c2.Real;
+            var last = c1.Imaginary * c2.Imaginary;
+
+            return new Complex(first - last, outer + inner);
+        }
+
+        private static IComplex Multiply(this IComplex c, double scalar)
+        {
+            return new Complex(c.Real * scalar, c.Imaginary * scalar);
         }
 
         /// <summary>
@@ -48,7 +56,11 @@ namespace ExtensionMethods
         /// <returns>the quotient.</returns>
         public static IComplex Divide(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            var realNumerator = c1.Real * c2.Real + c1.Imaginary * c2.Imaginary;
+            var imaginaryNumerator = c1.Imaginary * c2.Real - c1.Real * c2.Imaginary;
+            var denominator = c2.Real * c2.Real + c2.Imaginary * c2.Imaginary;
+
+            return new Complex(realNumerator / denominator, imaginaryNumerator / denominator);
         }
 
         /// <summary>
@@ -62,10 +74,7 @@ namespace ExtensionMethods
         /// </remarks>
         /// <param name="c1">the complex operand.</param>
         /// <returns>the complex conjugate.</returns>
-        public static IComplex Conjugate(this IComplex c1)
-        {
-            throw new NotImplementedException();
-        }
+        public static IComplex Conjugate(this IComplex c1) => new Complex(c1.Real, -c1.Imaginary);
 
         /// <summary>
         /// Get the reciprocal of a complex number.
@@ -80,7 +89,7 @@ namespace ExtensionMethods
         /// <returns>the complex reciprocal.</returns>
         public static IComplex Reciprocal(this IComplex c1)
         {
-            throw new NotImplementedException();
+            return c1.Conjugate().Multiply(1 / (c1.Modulus * c1.Modulus));
         }
     }
 }
