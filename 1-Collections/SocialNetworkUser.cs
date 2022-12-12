8ae61a6adaeab3e885b2da77bad314bb90c86a98
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-12-12 13:06:33 +0100

    Implemented User and SocialNetworkUser.

--- 1-Collections/SocialNetworkUser.cs
+++ 1-Collections/SocialNetworkUser.cs
@@ -1,32 +1,37 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 
 namespace Collections
 {
     public class SocialNetworkUser<TUser> : User, ISocialNetworkUser<TUser>
         where TUser : IUser
     {
+        private readonly Dictionary<string, List<TUser>> _followedUsers = new();
+
         public SocialNetworkUser(string fullName, string username, uint? age) : base(fullName, username, age)
         {
-            throw new NotImplementedException("TODO is there anything to do here?");
         }
 
         public bool AddFollowedUser(string group, TUser user)
         {
-            throw new NotImplementedException("TODO add user to the provided group. Return false if the user was already in the group");
-        }
-
-        public IList<TUser> FollowedUsers
-        {
-            get
+            // Trying to get the reference to the list contained as the Dictionary value.
+            if (_followedUsers.TryGetValue(group, out var list))
             {
-                throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
+                if (list.Contains(user)) return false;
+                list.Add(user);
+                return true;
             }
+
+            // Initialising list for the first time.
+            _followedUsers.Add(group, new List<TUser> {user});
+            return true;
         }
 
-        public ICollection<TUser> GetFollowedUsersInGroup(string group)
-        {
-            throw new NotImplementedException("TODO construct and return a collection containing of all users followed by the current users, in group");
-        }
+        public IList<TUser> FollowedUsers => _followedUsers.Values.ToList() as IList<TUser>;
+
+        public ICollection<TUser> GetFollowedUsersInGroup(string group) =>
+            _followedUsers.ContainsKey(group) ? _followedUsers[group] : new List<TUser>();
     }
 }
--- 1-Collections/User.cs
+++ 1-Collections/User.cs
@@ -6,18 +6,20 @@ namespace Collections
     {
         public User(string fullName, string username, uint? age)
         {
-            throw new NotImplementedException("TODO ensure that username is not null");
-            throw new NotImplementedException("TODO initialise this instance of user accordingly");
+            FullName = fullName;
+            if (username is not null)
+            {
+                Username = username;
+            }
+            Age = age;
         }
-        
+
         public uint? Age { get; }
-        
+
         public string FullName { get; }
-        
+
         public string Username { get; }
 
-        public bool IsAgeDefined => throw new NotImplementedException("TODO check whether age is non-null or not");
-        
-        // TODO implement missing methods (try to autonomously figure out which are the necessary methods)
+        public bool IsAgeDefined => Age is not null;
     }
 }
